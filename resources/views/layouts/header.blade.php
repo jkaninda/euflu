1da75ef942a768d8c3232a00b64982745f1e012a
jkanTech <jkantech@gmail.com>
2021-06-15 21:08:48 +0200

    Initial commit

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,163 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="icon" href="images/fav.png" type="image/png">
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/custom.css">
+<link rel="stylesheet" href="css/responsive.css">
+<link rel="stylesheet" href="css/color.css">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/fontawesome.css">
+<link rel="stylesheet" href="css/owl.carousel.min.css">
+<link rel="stylesheet" href="css/prettyPhoto.css">
+<!--Rev Slider Start-->
+<link rel="stylesheet" href="js/rev-slider/css/settings.css"  type='text/css' media='all' />
+<link rel="stylesheet" href="js/rev-slider/css/layers.css"  type='text/css' media='all' />
+<link rel="stylesheet" href="js/rev-slider/css/navigation.css"  type='text/css' media='all' />
+
+<!--Rev Slider End-->
+<title>Tigers Sports HTML Template</title>
+</head>
+<body>
+<!--Wrapper Start-->
+<div class="wrapper"> 
+  <!--Header Start-->
+  <header id="main-header" class="main-header"> 
+    <!--topbar-->
+    <div class="topbar">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-sm-6">
+            <ul class="topsocial">
+              <li><a href="index.html#" class="fb"><i class="fab fa-facebook-f"></i></a></li>
+              <li><a href="index.html#" class="tw"><i class="fab fa-twitter"></i></a></li>
+              <li><a href="index.html#" class="insta"><i class="fab fa-instagram"></i></a></li>
+              <li><a href="index.html#" class="in"><i class="fab fa-linkedin-in"></i></a></li>
+              <li><a href="index.html#" class="yt"><i class="fab fa-youtube"></i></a></li>
+            </ul>
+          </div>
+          <div class="col-md-6 col-sm-6">
+            <ul class="toplinks">
+              <li class="lang-btn">
+                <div class="dropdown">
+                  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> ENG </button>
+                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton"> <a class="dropdown-item" href="index.html#">ENG</a> <a class="dropdown-item" href="index.html#">FR</a> <a class="dropdown-item" href="index.html#">GR</a> <a class="dropdown-item" href="index.html#">AR</a> </div>
+                </div>
+              </li>
+              <li class="currency-btn">
+                <div class="dropdown">
+                  <button class="btn btn-secondary dropdown-toggle" type="button" id="currencydropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> USD </button>
+                  <div class="dropdown-menu" aria-labelledby="currencydropdown"> <a class="dropdown-item" href="index.html#">USD</a> <a class="dropdown-item" href="index.html#">Euro</a> <a class="dropdown-item" href="index.html#">Pound</a> </div>
+                </div>
+              </li>
+              <li class="acctount-btn"> <a href="index.html#">My Account</a> </li>
+              <li class="search-btn"> <a class="search-icon" href="index.html#search"><i class="fas fa-search"></i></a> </li>
+            </ul>
+            
+            <div id="search">
+                  <button type="button" class="close">×</button>
+                  <form class="search-overlay-form">
+                    <input type="search" value="" placeholder="type keyword(s) here" />
+                    <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i></button>
+                  </form>
+                </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--topbar end--> 
+    <!--Logo + Navbar Start-->
+    <div class="logo-navbar">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-2 col-sm-5">
+            <div class="logo"><a href="index.html"><img src="images/logo-dark.png" alt=""></a></div>
+          </div>
+          <div class="col-md-10 col-sm-7">
+            <nav class="main-nav">
+              <ul>
+                <li class="nav-item drop-down"> <a href="index.html">Home</a>
+                  <ul>
+                    <li><a href="index.html">Home One</a></li>
+                    <li><a href="index-two.html">Home Two</a></li>
+                </ul>
+                </li>
+                <li class="nav-item drop-down"> <a href="index.html">Matches</a>
+                  <ul>
+                    <li><a href="upcoming-match.html">Upcoming Match</a></li>
+                    <li><a href="match-resut-layout-1.html">Match Result One</a></li>
+                    <li><a href="match-resut-layout-2.html">Match Result Two</a></li>
+                    <li><a href="match-resut-layout-3.html">Match Result Three</a></li>
+                    <li><a href="match-details.html">Match Details</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item drop-down"> <a href="index.html">Players</a>
+                  <ul>
+                    <li><a href="team-one.html">Team One</a></li>
+                    <li><a href="team-two.html">Team Two</a></li>
+                    <li><a href="team-three.html">Team Three</a></li>
+                    <li><a href="team-four.html">Team Four</a></li>
+                    <li><a href="team-details.html">Team Details</a></li>
+                    <li><a href="staff-details.html">Staff Details</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item drop-down"> <a href="index.html">Features</a>
+                  <ul>
+                    <li class="drop-down"> <a href="index.html#">Shop</a>
+                      <ul>
+                        <li><a href="products-grid.html">Product Grid</a></li>
+                        <li><a href="products-grid-two.html">Product Grid Two</a></li>
+                        <li><a href="products-grid-three.html">Product Grid Three</a></li>
+                        <li><a href="products-list.html">Product List</a></li>
+                        <li><a href="shop-details.html">Shop Details</a></li>
+                      </ul>
+                    </li>
+                    <li class="drop-down"> <a href="index.html#">Gallery</a>
+                      <ul>
+                        <li><a href="gallery-2-col.html">Gallery Two Col</a></li>
+                        <li><a href="gallery-3-col.html">Gallery Three Col</a></li>
+                        <li><a href="gallery-clasic.html">Gallery Classic</a></li>
+                        <li><a href="gallery-massonry.html">Gallery Massonry</a></li>
+                        <li><a href="gallery-modern.html">Galery Modern</a></li>
+                      </ul>
+                    </li>
+                    <li><a href="groups.html">Groups</a></li>
+                    <li><a href="page-404.html">Page 404</a></li>
+                    <li><a href="page-404-one.html">Page 404 Two</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item"> <a href="point-table.html">Point Table</a> </li>
+                <li class="nav-item drop-down"> <a href="index.html">News</a>
+                  <ul>
+                    <li><a href="news-grid.html">News Grid</a></li>
+                    <li><a href="news-large.html">News Large</a></li>
+                    <li><a href="news-list.html">News List</a></li>
+                    <li><a href="news-details.html">News Details</a></li>
+                  </ul>
+                </li>
+				<li class="nav-item drop-down"> <a href="index.html">Fixtures</a>
+                  <ul>
+                    <li><a href="fixtures-layout-1.html">Fixtures One</a></li>
+                    <li><a href="fixtures-layout-2.html">Fixtures Two</a></li>
+                   
+                  </ul>
+                </li>
+                
+                <li class="nav-item drop-down"> <a href="index.html">Contact</a>
+                  <ul>
+                    <li><a href="contact.html">Contact One</a></li>
+                    <li><a href="contact-two.html">Contact Two</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item buy-ticket"> <a href="index.html#">Buy Tickets</a> </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--Logo + Navbar End--> 
+  </header>
